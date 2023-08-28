ceefcfa40a2572fdc44d3211245eb028d3a4b0a6
StephanieBruno <stephaniebruno08@gmail.com>
2023-08-28 23:58:50 +0530

    Assingment

---
+++ project/ProjectDeSeriailizer.java
@@ -0,0 +1,33 @@
+package project;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.Map;
+
+
+public class ProjectDeSeriailizer {
+
+			Map<Project,ArrayList<Employee>> employee;
+
+		    public Map<Project,ArrayList<Employee>> projectDeSeriailizer() {
+			try {
+				FileInputStream fileInput=new FileInputStream("C:\\Users\\MY PC\\eclipse-workspace\\Project1\\src\\com\\gradedproject\\q1\\file.txt");
+				ObjectInputStream in=new ObjectInputStream(fileInput);
+				employee=(Map<Project, ArrayList<Employee>>) in.readObject();
+			}catch(FileNotFoundException e) {
+				e.printStackTrace();
+			}catch(ClassNotFoundException cnfe) {
+				cnfe.printStackTrace();
+			}catch(IOException ioe) {
+				ioe.printStackTrace();
+			}
+			
+			return employee;
+
+		}
+
+		}
+
+	
